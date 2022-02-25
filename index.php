494298bd76553ff8e7af04836a31bfc5a1c1e471
Rogério Rodrigues <rogeriosfa@gmail.com>
2022-02-25 03:08:01 -0300

    adicionando formulario para dados do usuário

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <?php require("css.php"); ?>
-    
+
     <title>API para Sorteios da Monetizze</title>
 </head>
 
@@ -17,6 +17,59 @@
         <div class="row">
 
             <h3 class="header center-align">Bem-vindo a API de Sorteios da Monetizze</h3>
+            <br>
+            <br>
+
+            <form action="sorteio.php" method="GET">
+                <h5 class="center-align">Quantas dezenas você deseja tripulante?</h5>
+
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input name="dezenas" type="number" class="validate" min="6" max="10">
+                        <label class="active" for="first_name2">Escolha entre 6 a 10:</label>
+                    </div>
+
+                    <?php if (isset($_GET['alert_dezena'])) : ?>
+                        <div class="row">
+                            <div class="col s12 m12">
+                                <div class="card-panel red darken-1">
+                                    <span class="white-text">Nº de dezenas não permitido, escolha novamente!
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endif; ?>
+
+                </div>
+
+                <h5 class="center-align">e quantos bilhetes você vai desejar?</h5>
+
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input name="bilhetes" type="number" class="validate" min="1" max="50">
+                        <label class="active" for="first_name2">Escolha entre 1 a 50:</label>
+                    </div>
+
+                    <?php if (isset($_GET['alert_bilhete'])) : ?>
+                        <div class="row">
+                            <div class="col s12 m12">
+                                <div class="card-panel red darken-1">
+                                    <span class="white-text">Nº de bilhetes não permitido, escolha novamente!
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endif; ?>
+
+                </div>
+
+                <div>
+                    <button class="btn waves-effect waves-light" type="submit" name="action">Enviar
+                        <i class="material-icons right">send</i>
+                    </button>
+                </div>
+
+            </form>
 
         </div>
     </div>
