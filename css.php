1bec91b6f9cf61f01122f0596dbab506ab7643ed
Rogério Rodrigues <rogeriosfa@gmail.com>
2022-02-25 03:02:28 -0300

    melhorias na organização do fonts

---
+++ css.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link href="/css/styles.css" rel="stylesheet">
+<link href="https://materializecss.com/css/ghpages-materialize.css" rel="stylesheet">
+<style>
+    .grid-example.active {
+        background-color: #009688 !important;
+    }
+</style>
--- index.php
+++ index.php
@@ -6,9 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
+    <?php require("css.php"); ?>
+    
     <title>API para Sorteios da Monetizze</title>
 </head>
 
@@ -23,6 +22,6 @@
     </div>
 </body>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<?php require("scripts.php") ?>
 
 </html>
